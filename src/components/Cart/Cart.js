4cf669b95781b5168d789b1b88bf79fe0f749ca6
Tony Kowalewski <tony@kowalewski.dev>
2024-04-12 00:47:20 +0200

    add empty cart placeholder logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,20 +7,18 @@ const Cart = (props) => {
   const cartItems = useSelector((store) => store.products);
   const cartIsEmpty = cartItems.length === 0;
 
-  //  add logic to show text for empty cart
-
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
-
-      <ul>
-        {/* <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        /> */}
-        {cartItems.map((item) => {
-          return <CartItem key={item.title} item={{ ...item }} />;
-        })}
-      </ul>
+      {cartIsEmpty ? (
+        <p>No Items</p>
+      ) : (
+        <ul>
+          {cartItems.map((item) => {
+            return <CartItem key={item.title} item={{ ...item }} />;
+          })}
+        </ul>
+      )}
     </Card>
   );
 };
